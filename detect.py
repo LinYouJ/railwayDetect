5892c681f5c376400060798256a545934004ce91
Jacky Lin <a09795656101@gmail.com>
2022-12-17 22:35:49 +0800

    first commit

--- detect.py
+++ detect.py
@@ -7,20 +7,30 @@ def maskROI(img):
     cv2.fillPoly(mask, [coordinate], (255, 255, 255))
     mask_img = cv2.bitwise_and(img, mask)
     return mask_img, mask
+    
 
-while True: 
-    oriImg = cv2.imread(r'railwayDist/railway.png')
-    Img = cv2.resize(oriImg, (700, 500))
-    imgGray = cv2.cvtColor(Img, cv2.COLOR_BGR2GRAY)
-    imgBlur = cv2.GaussianBlur(imgGray, (7, 7), 0, 0)
-    imgCan = cv2.Canny(imgBlur, 171, 57)
+oriImg = cv2.imread('railway.png')
+Img = cv2.resize(oriImg, (700, 500))
+imgGray = cv2.cvtColor(Img, cv2.COLOR_BGR2GRAY)
+imgBlur = cv2.GaussianBlur(imgGray, (7, 7), 0, 0)
+imgCan = cv2.Canny(imgBlur, 171, 57)
 
-    imgMask, mask = maskROI(imgCan)
+imgMask, mask = maskROI(imgCan)
 
-    #cv2.imshow('img', Img)
-    #cv2.imshow('imgCan', imgCan)
-    cv2.imshow('mask', imgMask)
-    cv2.imshow('imgmask', mask)
-    if cv2.waitKey(1) == ord('q'):
-        cv2.destroyAllWindows()
-        break
+Lines = cv2.HoughLinesP(imgMask, 1, np.pi/180, 100, minLineLength=100, maxLineGap=6)
+for line in Lines:
+    x1, y1, x2, y2 = line[0]
+    cv2.line(Img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+    m = (y2 - y1)/(x2 - x1)
+    if m > 0:
+        print(f'left:{m}')
+    else :
+        print(f'right:{m}')
+
+cv2.imshow('mask', imgMask)
+cv2.imshow('Img', Img)
+
+if cv2.waitKey(0) == ord('q'):
+    cv2.destroyAllWindows()
+        
+    
